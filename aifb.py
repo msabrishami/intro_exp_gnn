aaab103183d7c65d080c2b723ce43da59ec43098
Saeed Abrishami <abri442@usc.edu>
2020-04-16 12:40:58 -0700

    aifb module returns torch_geometric.data.in_memory_dataset object

--- aifb.py
+++ aifb.py
@@ -3,6 +3,7 @@ import os
 import numpy as np
 import scipy.sparse as sp
 import pdb
+import torch
 from torch_geometric.data import InMemoryDataset, Data
 
 ''' More simple approach:
@@ -105,7 +106,7 @@ class AIFB(object):
         test_idx_file   = os.path.join(self.path, 'test_idx.npy')
         
         all_edges = np.load(edge_file)
-        self.num_node = all_edges['n'].item()
+        self.num_nodes = all_edges['n'].item()
         self.edge_list = all_edges['edges']
         self.num_rel = all_edges['nrel'].item()
 
@@ -115,11 +116,83 @@ class AIFB(object):
         self.train_idx = np.load(train_idx_file)
         self.test_idx = np.load(test_idx_file)
 
-        print('\t- Number of nodes: ', self.num_node)
+        print('\t- Number of nodes: ', self.num_nodes)
         print('\t- Number of edges: ', len(self.edge_list))
         print('\t- Number of relations: ', self.num_rel)
         print('\t- Number of classes: ', self.labels.shape[1])
-
     
-    def get_pytorch_geometric_dataset(self):
-        dataset = Data
+    def preprocess(self, bfs_level=3, relabel=False):
+        
+        if bfs_level > 0:
+            print("Removing nodes that are more than {} hops away".format(bfs_level))
+            row, col, edge_type = self.edge_list.transpose()
+            A = sp.csr_matrix((np.ones(len(row)), (row, col)), shape=(self.num_nodes, self.num_nodes))
+            bfs_generator = _bfs_relational(A, self.labeled_nodes_idx)
+            lvls = list()
+            lvls.append(set(self.labeled_nodes_idx))
+            for _ in range(bfs_level):
+                lvls.append(next(bfs_generator))
+            to_delete = list(set(range(self.num_nodes)) - set.union(*lvls))
+            eid_to_delete = np.isin(row, to_delete) + np.isin(col, to_delete)
+            eid_to_keep = np.logical_not(eid_to_delete)
+            self.edge_src = row[eid_to_keep]
+            self.edge_dst = col[eid_to_keep]
+            self.edge_type = edge_type[eid_to_keep]
+
+            if relabel:
+                uniq_nodes, edges = np.unique((self.edge_src, self.edge_dst), return_inverse=True)
+                self.edge_src, self.edge_dst = np.reshape(edges, (2, -1))
+                node_map = np.zeros(self.num_nodes, dtype=int)
+                self.num_nodes = len(uniq_nodes)
+                node_map[uniq_nodes] = np.arange(self.num_nodes)
+                self.labels = self.labels[uniq_nodes]
+                self.train_idx = node_map[self.train_idx]
+                self.test_idx = node_map[self.test_idx]
+                print("{} nodes left".format(self.num_nodes))
+        else:
+            self.edge_src, self.edge_dst, self.edge_type = edges.transpose()
+
+        # normalize by dst degree
+        _, inverse_index, count = np.unique((self.edge_dst, self.edge_type), axis=1, return_inverse=True, return_counts=True)
+        degrees = count[inverse_index]
+        self.edge_norm = np.ones(len(self.edge_dst), dtype=np.float32) / degrees.astype(np.float32)
+
+        # convert to pytorch label format
+        self.num_classes = self.labels.shape[1]
+        self.labels = np.argmax(self.labels, axis=1)
+
+
+class AIFB_pyg(InMemoryDataset):
+
+    def __init__(self, transform=None):
+        super(AIFB_pyg, self).__init__(".", transform, None, None)
+        ds = AIFB()
+        ds.load_data()
+        ds.preprocess()
+        src_tensor = torch.from_numpy(ds.edge_src.astype(np.int64)).to(torch.long)
+        dst_tensor = torch.from_numpy(ds.edge_dst.astype(np.int64)).to(torch.long)
+        edge_index = torch.stack([src_tensor, dst_tensor], dim=0)
+        data = Data(edge_index=edge_index)
+
+        data.num_nodes = ds.num_nodes   # need to be checked after preprocess
+        # data.x = torch.eye(data.num_nodes, dtype=torch.float)
+        data.x = torch.ones(data.num_nodes, 1)
+        data.y = torch.tensor(ds.labels)
+        data.train_mask = torch.tensor([False] * data.num_nodes)
+        data.test_mask = torch.tensor([False] * data.num_nodes)
+        data.train_mask[ds.train_idx] = True
+        data.test_mask[ds.test_idx] = True
+        self.data = data
+        # self.data, self.slices = self.collate([data])
+
+    def _download(self):
+        return
+
+    def _process(self):
+        return
+
+    def __repr__(self):
+        return '{}()'.format(self.__class__.__name__)
+
+
+
